a67e864afd9c7c700e1d80330f36491ecd91c857
David Murphy <david@murphytech.ie>
2019-06-05 22:11:08 +0100

    Demoed servo control software using MQTT

--- Servo_Ctrl/servoCtrl.py
+++ Servo_Ctrl/servoCtrl.py
@@ -1,142 +1,106 @@
 
 #!/usr/bin/python
-#from Tkinter import *
-from Adafruit_PWM_Servo_Driver import PWM
-import time
-import socketio
-import eventlet
-from flask import Flask, render_template
+'''
+Servo Control code.
+Listens to Android device sensor data input and actuates camera system dual-axiz servos accordingly.
 
-sio = socketio.Server()
-app = Flask(__name__, template_folder='template')
+David Murphy
+d.murphy53@nuigalway.ie
+MECE - 12493252
+'''
+import os, sys, time
+import paho.mqtt.client as mqtt
+from Adafruit_I2C import Adafruit_PWM_Servo_Driver as i2c
 
-servos = {
-    0 : {'name' : 'pan', 'angle' : 90},
-    1 : {'name' : 'tilt', 'angle' : 90},
-}
+#Constants file
+import constant
 
+#Setup Servo HAT
+pwm = i2c.PWM(0x41)
 
-# Load the main form template on webrequest for the root page
-@app.route("/")
-def index():
-    """Serve the client-side application"""
+# The callback for when the client receives a CONNACK response from the server.
+def on_connect(client, userdata, flags, rc):
+    if rc==0:
+        print("connected OK, Returned code: ",rc)
+    else:
+        print("Bad connection, Returned code: " ,rc)
 
-    # Create a template data dictionary to send any data to the template
-    templateData = {
-        'title' : 'PiServos'
-        }
-    # Pass the template data into the template piservos.html and return it to the user
-    return render_template('piservos.html', **templateData)
+    print("Connected with result code "+str(rc))
+    # Subscribing in on_connect() means that if we lose the connection and
+    # reconnect then subscriptions will be renewed.
+    client.subscribe("servoEvent", 0)
+    return
 
-@sio.on('connect')
-def connect(sid, environ):
-    print('connect', sid)
+# The callback for when a PUBLISH message is received from the server.
+def on_message(client, userdata, msg):
+    timeStamp = time.asctime(time.localtime(time.time()))
+    print(msg.topic+" " + str(msg.payload) + " @Time: " + timeStamp)
+    panAngle, tiltAngle = str(msg.payload).split(",")
+    print type(panAngle)
+    print type(tiltAngle)
+    servoEvent(float(panAngle), float(tiltAngle), timeStamp)
 
-@sio.on('servoEvent')
-def message(sid, panAngle, tiltAngle):
-    pwm.setPWM(0, 0, angleMap(int(panAngle)))
-    print('PAN: ', int(panAngle))
-    pwm.setPWM(1, 0, angleMap(int(tiltAngle)))
-    print('TILT: ', int(tiltAngle))
+def on_log(mqttc, obj, level, string):
+    print(string)
 
-@sio.on('servoPanEvent')
-def message(sid, panAngle):
-    pwm.setPWM(0, 0, angleMap(int(panAngle)))
-    print('PAN: ', int(panAngle))
+def servoEvent(panAngle, tiltAngle, timeStamp):
+    print("Time: " + str(timeStamp) + "\tPan: " + str(panAngle) + "\tTilt: " + str(tiltAngle))
+    print("ZERO: " + str(mapPanServo(panAngle)) + " = " + str(panAngle) + "\tONE: " + str(mapTiltServo(tiltAngle)) + " = " + str(tiltAngle))
+    #global pwm
+    pwm.setPWM(constant.PANSERVO, constant.ZEROTICK, mapPanServo(panAngle))
+    pwm.setPWM(constant.TILTSERVO, constant.ZEROTICK, mapTiltServo(tiltAngle))
 
-@sio.on('servoTiltEvent')
-def message(sid, tiltAngle):
-    pwm.setPWM(1, 0, angleMap(int(tiltAngle)))
-    print('TILT: ', int(tiltAngle))
+def mapPanServo(angle):
+    print("mapPanServo")
+    # y = 661.3 + (-2.89)*x
+    # x = Angle in degrees
+    # y = correcponding PWM value
+    if angle < 4:
+        angle = 4
+    if angle > 176:
+	angle = 176
+    print("Pan:" + str(angle))
+    return int(round(constant.YINTERCEPTZERO + (constant.SLOPEZERO * int(round(angle)))))
 
-@sio.on('disconnect')
-def disconnect(sid):
-    print('disconnect', sid)
+def mapTiltServo(angle):
+    print("mapTiltServo")
+    # y = 601.6 + (-2.66)*x
+    # x = Angle in degrees
+    # y = correcponding PWM value
+    if angle < 20:
+        angle = 20
+    if angle > 176:
+       angle = 176
+    print("Tilt" + str(angle))
+    return int(round(constant.YINTERCEPTONE + (constant.SLOPEONE * int(round(angle)))))
 
-# Initialise the PWM device using the default address
-pwm = PWM(0x40)
-# Note if you'd like more debug output you can instead run:
-#pwm = PWM(0x40, debug=True)
+def setupServos():
+    #global pwm
+    #pwm = i2c.PWM(0x41)
+    pwm.setPWMFreq(60)
+    pwm.setPWM(constant.PANSERVO, constant.ZEROTICK, constant.PANINITIAL)
+    pwm.setPWM(constant.TILTSERVO, constant.ZEROTICK, constant.TILTINITIAL)
 
-servoMin = 500  # Min pulse length out of 4096
-servoMax = 600
-servoStep = 50  # Max pulse length out of 4096
-servoCurrentPan = 0
-servoCurrentTilt = 0
+def main():
+    setupServos()
+    client = mqtt.Client()
+    client.on_connect = on_connect
+    client.on_message = on_message
+    client.on_log = on_log
+    print("Calling connect...")
+    client.connect("192.168.1.253", 1883)
+    print("After connection attempt")
 
-leftPos = 0.5
-rightPos = 2.5
-middlePos = (leftPos + rightPos)/2 + leftPos
+    # Blocking call that processes network traffic, dispatches callbacks and
+    # handles reconnecting.
+    # Other loop*() functions are available that give a threaded interface and a
+    # manual interface.
+    client.loop_forever()
 
-positionList = [leftPos, middlePos, rightPos]
-msPerCycle = 1000 / 60
-
-
-def setServoPulse(channel, pulse):
-    """PWM setup"""
-    pulseLength = 1000000                   # 1,000,000 us per second
-    #pulseLength = 10000
-    pulseLength /= 60                       # 60 Hz
-    print "%d us per period" % pulseLength
-    pulseLength /= 4096                     # 12 bits of resolution
-    print "%d us per bit" % pulseLength
-    pulse *= 1000
-    pulse /= pulseLength
-    pwm.setPWM(channel, 0, pulse)
-
-pwm.setPWMFreq(60)
-
-
-print('Moving servo on channel 0, press Ctrl-C to quit...')
-left = 122
-middle = 366
-right = 610
-
-def angleMap(angle):
-    return int((round((488.0/180.0),0)*angle) + 122)
-
-@app.route("/<direction>")
-def move(direction):
-    if direction == 'left':
-        print("Left...")
-        #Increment Angle
-        newAngle = servos[0]['angle'] + 5
-        #Check if valid
-        if newAngle <= 180:
-            pwm.setPWM(0, 0, angleMap(newAngle))
-            servos[0]['angle'] = newAngle
-
-    elif direction == 'right':
-        print("Right...")
-        #Decrement Angle
-        newAngle = servos[0]['angle'] - 5
-        #Check if valid
-        if newAngle >= 0:
-            pwm.setPWM(0, 0, angleMap(newAngle))
-            servos[0]['angle'] = newAngle
-
-    elif direction == 'up':
-        print("Up...")
-        #Increment Angle
-        newAngle = servos[1]['angle'] + 5
-        #Check if valid
-        if newAngle <= 180:
-            pwm.setPWM(1, 0, angleMap(newAngle))
-            servos[1]['angle'] = newAngle
-
-    elif direction == 'down':
-        print("Down...")
-        #Decrement Angle
-        newAngle = servos[1]['angle'] - 5
-        #Check if valid
-        if newAngle >= 0:
-            pwm.setPWM(1, 0, angleMap(newAngle))
-            servos[1]['angle'] = newAngle
-
-
-if __name__ == "__main__":
-	#wrap Flask application with socketio's middleware
-        app = socketio.Middleware(sio, app)
-
-        #deploy as an eventlet WSGI server
-        eventlet.wsgi.server(eventlet.listen(('', 8088)),app)
+if __name__=="__main__":
+    try:
+        main()
+    except KeyboardInterrupt:
+        print("\nKeyboard Interrupt")
+        #Cleanup()
+        sys.exit(0)
